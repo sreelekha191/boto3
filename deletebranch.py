9d1d7a24bc6c9f97de59765a47bb72276b034752
sreelekha191 <sreelekha191@gmail.com>
2023-04-01 22:06:58 -0400

    Update deletebranch.py

--- deletebranch.py
+++ deletebranch.py
@@ -8,14 +8,14 @@ repo_path = "/Users/sreelekha/Documents/GitHub/boto3"
 repo = git.Repo.clone_from(repo_url,repo_path)
 
 #checkout the branch to be deleted
-branch_name="my_branch"
-repo.git.chechkout(test)
+branch_name = 'test'
+repo.git.chechkout(branch_name)
 
 #Merge the branch into the master branch_name
 repo.git.merge(branch_name)
 
-Delete the branch
-repo.git.branch("-d",branch_name)
+#Delete the branch
+repo.git.branch("-d", branch_name)
 
 #push the change to the remote repository
-repo.git.push("origin","master")
+repo.git.push("origin", "master")
